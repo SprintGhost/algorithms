f9162853f0de19a7617fd9e44d7a22e4484d61c7
MrSprint <18240069963@163.com>
2018-01-16 22:32:06 +0800

    finish exercise1.1.1

---
+++ src/chapter1_1/Ex03.java
@@ -0,0 +1,28 @@
+package chapter1_1;
+/*
+ * Write a program, get three int parameters from command line
+ * if they are equal then print
+ * else print not equal
+ */
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex03 {
+    public static void main(String[] args){
+        int[] par = new int[3];
+        if (args.length == 3) {
+            for (int i = 0; i <args.length; i++) {
+                par[i]  = Integer.parseInt(args[i]);
+            }
+            if (par[0] == par[1]) {
+                if(par[0] == par[2]){
+                    StdOut.println("equal");
+                    System.exit(0);
+                }
+            }
+            StdOut.println("not equal");
+            System.exit(1);
+        }
+        StdOut.println("Illegal input");
+        System.exit(1);
+    }
+}

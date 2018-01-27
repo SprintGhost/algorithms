25eff478bd654abc6a31b0a69f52c96022e78e4b
MrSprint <18240069963@163.com>
2018-01-27 22:46:15 +0800

    Class TestVisualAccumulator in page 95

---
+++ src/chapter1_2/VisualAccumulator.java
@@ -0,0 +1,30 @@
+package chapter1_2;
+/*
+ * Class VisualAccumulator in page 95
+ *
+ */
+import edu.princeton.cs.algs4.StdDraw;
+
+public class VisualAccumulator {
+    private double total;
+    private int N;
+    public VisualAccumulator(int trials, double max){
+        StdDraw.setXscale(0, trials);
+        StdDraw.setYscale(0, max);
+        StdDraw.setPenRadius(.005);
+    }
+    public void addDatavalue(double val){
+        N++;
+        total += val;
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.point(N, val);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(N,total/N);
+    }
+    public double mean(){
+        return total/N;
+    }
+    public String toString(){
+        return "Mean (" + N + " values): " + String.format("%7.5f", mean());
+    }
+}

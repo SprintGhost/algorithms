caccbd58b4329a9cef6d1d16e91dbf7115931233
MrSprint <18240069963@163.com>
2018-04-13 23:47:13 +0800

    Update some sort function && add a draw function(going)

---
+++ src/chapter2_1/SelectSort.java
@@ -0,0 +1,89 @@
+/*************************************
+ # @Time    : 2018/4/13
+ # @Author  : Mr.Sprint
+ # @Project : algorithms
+ # @File    : SelectSort
+ # @Software: IntelliJ IDEA
+ # code is far away from bugs with the god animal protecting
+ I love animals. They taste delicious.
+          ┏┓      ┏┓
+         ┏┛┻━━━━━━┛┻┓
+         ┃    ☃     ┃
+         ┃ ┳┛   ┗┳  ┃
+         ┃    ┻     ┃
+         ┗━┓      ┏━┛
+           ┃      ┗━━┓
+           ┃ 神兽保佑 ┣┓
+           ┃永无BUG！┏┛
+           ┗┓┓┏━┳┓┏┛
+            ┃┫┫  ┃┫┫
+            ┗┻┛  ┗┻┛
+ **************************************/
+package chapter2_1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SelectSort {
+    public static void Select_Sort(Comparable[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+        {
+            int min = i;
+            for(int j = i + 1; j < a.length; j++)
+            {
+                if(less(a[j], a[min])) min = j;
+                exch(a, i, min);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j)
+    {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    private static void draw(String[] a)
+    {
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        for (int i = 0; i < a.length; i++)
+            StdDraw.rectangle( 2 * i + 1,0, 1, Double.parseDouble(a[i]));
+    }
+
+    public static boolean isSorted(Comparable[] a)
+    {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        StdDraw.setXscale(0, 10);
+        StdDraw.setYscale(0, 5);
+        StdDraw.setPenRadius(.005);
+//        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        String[] a = StdIn.readAllStrings();
+//        StdDraw.filledRectangle(0.0,0, Double.parseDouble(a[0]), 2);
+        SelectSort.Select_Sort(a);
+        assert isSorted(a);
+        show(a);
+        draw(a);
+    }
+}
---
+++ src/chapter2_1/SortCompare.java
@@ -0,0 +1,25 @@
+/*************************************
+ # @Time    : 2018/4/13
+ # @Author  : Mr.Sprint
+ # @Project : algorithms
+ # @File    : SortCompare
+ # @Software: IntelliJ IDEA
+ # code is far away from bugs with the god animal protecting.
+ # I love animals. They taste delicious.
+          ┏┓      ┏┓
+         ┏┛┻━━━━━━┛┻┓
+         ┃    ☃     ┃
+         ┃ ┳┛   ┗┳  ┃
+         ┃    ┻     ┃
+         ┗━┓      ┏━┛
+           ┃      ┗━━┓
+           ┃ 神兽保佑 ┣┓
+           ┃ 永无BUG！┏┛
+           ┗┓┓┏━┳┓┏┛
+            ┃┫┫  ┃┫┫
+            ┗┻┛  ┗┻┛
+ **************************************/
+package chapter2_1;
+
+public class SortCompare {
+}
